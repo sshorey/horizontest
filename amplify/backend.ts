34b1054c5534929f4d2c83f8f111ddd470cd4926
sshorey <69634408+sshorey@users.noreply.github.com>
2024-11-10 19:53:23 -0600

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource.js';
-import { data } from './src/data/mockData.js';
+import { data } from 'https://github.com/sshorey/horizontest/tree/9922f752b624ef7b901708dc1dff044108439571/src/data';
 
 defineBackend({
   auth,
